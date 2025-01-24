a7a974d7ceb4866b13bd03d060a65804f6116709
Alejandro Perezgit config --global user.name Alejandro <alejandro.perezsanabria@dcmail.ca>
2025-01-24 08:11:19 -0500

    initial commit

---
+++ TCA.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+import os
+
+# Path to the Chrome WebDriver (relative path)
+webdriver_path = os.path.join('chromedriver-win64', 'chromedriver.exe')
+# Set up the Chrome WebDriver service
+service = ChromeService(webdriver_path)
+
+# Set Chrome options
+options = Options()
+options.add_argument('--start-maximized')
+
+# Initialize the Chrome WebDriver
+driver = webdriver.Chrome(service=service, options=options)
+
+# Open a website
+driver.get('https://login.myameego.com/')
+
+# Perform actions on the website
+# Example: Find an element by its name and print its text
+title = driver.title
+print(title)
+
+time.sleep(10)
+
+# Close the browser
+driver.quit()
