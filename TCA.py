f5e41e09b2cbeaa5ffe56d9a563d37f63945900f
Alejandro Perezgit config --global user.name Alejandro <alejandro.perezsanabria@dcmail.ca>
2025-02-04 06:10:00 -0500

    the script now fully works, it can now scrape the ameego website to get the timesheet for the day and also be a ble to tell which workers need a break entered in the system. Next in line it will be to make the script be able to modify the timesheet and add the breaks by itself

--- TCA.py
+++ TCA.py
@@ -4,6 +4,6 @@ from data import ameegoData
 from navigation import clearviewScrapper, ameegoScrapper
 
 
-data = ameegoData.load_data('employee_shifts_2025-01-24.csv')
+
 ameegoScrapper.ameegoScrap()
 
--- navigation/ameegoScrapper.py
+++ navigation/ameegoScrapper.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 import os
 import constants
+import time
+from datetime import timedelta
+import pandas as pd
 def ameegoScrap():
     # Path to the Chrome WebDriver (relative path)
     webdriver_path = os.path.join('chromedriver-win64', 'chromedriver.exe')
@@ -37,15 +40,67 @@ def ameegoScrap():
     # Find the sign in button
     sign_in_button = driver.find_element('css selector', 'button[type="submit"]')
     sign_in_button.click()
+    time.sleep(5)
+    # uses driver.get to navigate tot he time sheet for the day
+    driver.get('https://c.myameego.com/manager/time-and-attendance.php?date='+str(date.today() - timedelta(days=1)))
+
+    # the site saves all the schedule data in a table so we find the table and get the info from it
+    table = driver.find_element('css selector', 'table')
+    rows = table.find_elements('tag name', 'tr')
     
-    # prints a console message tht will run driver.quit() if q is entered
-    ###########################################################################
-    # TODO
-    # use driver.get('https://c.myameego.com/manager/payroll-export/')
-    # get the current date
-    # Returns the current local date
-    today = date.today()
-    print("Today date is: ", today)
+    # Create a dictionary to store the data
+    schedule_data = {}
+    
+
+    # Iterate through the rows and extract the data
+    for row in rows[1:]:  # Skip the header row
+        cells = row.find_elements('tag name', 'td')
+        if len(cells) > 1:
+            name = cells[0].text
+            wage = cells[1].text
+            start_end = cells[2].text
+            length = cells[3].text
+            overtime = cells[4].text
+            weekly_overtime = cells[5].text
+            cost = cells[6].text
+            # Split the start_end value into start and end times
+            start_time, end_time = start_end.split('-')
+            # Store the data in the dictionary
+            schedule_data[name] = [name, wage, start_time, end_time, length, overtime, weekly_overtime, cost]
+
+          
+    # Convert the dictionary to a DataFrame
+    df = pd.DataFrame.from_dict(schedule_data, orient='index', columns=['name', 'Wage', 'Start', 'End', 'Length', 'Overtime', 'Weekly Overtime', 'Cost'])
+    # Convert 'Length' column to float hours
+    df['Length'] = df['Length'].apply(lambda x: float(x.split()[0]) if isinstance(x, str) else x)
+    df['Start'] = df['Start'].apply(lambda x: float(x.split(':')[0]) % 12 + (12 if 'pm' in x.lower() else 0) + float(x.split(':')[1][:2]) / 60)
+    df['End'] = df['End'].apply(lambda x: float(x.split(':')[0]) % 12 + (12 if 'pm' in x.lower() else 0) + float(x.split(':')[1][:2]) / 60)
+    
+    for i in df.index:
+        fullShift = df.loc[i]['End'] - df.loc[i]['Start']
+        lenght = df.loc[i]['Length']
+        #if the calculation is negative then that means its a midnight shift(this is how i will handle it for now)
+        if fullShift < 0:
+            breakTime = fullShift/2 + lenght
+            # Convert breaktime to positive
+            breakTime = abs(breakTime)
+        else:
+            breakTime = fullShift - lenght
+            
+        print(fullShift)
+        print(lenght)
+        print(breakTime)
+        if lenght >= 5.5 and breakTime != 0.5 :
+            print(df.loc[i]['name'] + " has a shift of more hours than 5 so please add break")
+        print(df.loc[i])
+
+    
+    
+    
+    # gets an input from the user tht will run driver.quit() if q is entered
+    quit = input("press q to quit")
+    if quit == 'q':
+        driver.quit()
     
     
     
